8629a245c1c46d6ff9b2d508427f86ee6defeec6
Ahmed Abdelhay <ahmedatta2108@gmail.com>
2025-10-30 15:55:47 +0100

    refactor: streamline imports and enhance PDF processing logic in CLI

--- src/indu_doc/__init__.py
+++ src/indu_doc/__init__.py
@@ -7,56 +7,7 @@ A Python package for processing and extracting structured data from industrial P
 __version__ = "0.0.1"
 __author__ = "Ahmed Abdelhay, Artsiom Drankevich"
 
-# Core classes - most commonly used
-from .god import God
-from .configs import AspectsConfig, LevelConfig
-
-# Data structures
-from .xtarget import XTarget, XTargetType
-from .connection import Connection, Pin, Link
-from .attributes import Attribute, AttributeType, SimpleAttribute, RoutingTracksAttribute
-from .tag import Tag
-
-# Page processing
-from .plugins.eplan_pdfs.common_page_utils import PageType, detect_page_type
-
-# Base classes
-from .attributed_base import AttributedBase
-
-# Searcher
-from .searcher import Searcher
-
-# CLI entry point
-from .cli import main as cli_main
-
 __all__ = [
-    # Core
-    "God",
-    "AspectsConfig",
-    "LevelConfig",
-    
-    # Data structures
-    "XTarget",
-    "XTargetType", 
-    "Connection",
-    "Pin",
-    "Link",
-    "Attribute",
-    "AttributeType",
-    "SimpleAttribute",
-    "RoutingTracksAttribute",
-    "Tag",
-    
-    # Page processing
-    "PageType",
-    "detect_page_type",
-    
-    # Searcher
-    "Searcher",
-    
-    # Base classes
-    "AttributedBase",
-    
-    # CLI
-    "cli_main",
+    "__version__",
+    "__author__",
 ]
